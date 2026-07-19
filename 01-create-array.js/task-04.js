13486e741a2f5e0139b04b9cbcc49ed2aac875e0
Aminul islam <aminulislam424842@gmail.com>
2026-07-19 21:41:55 +0600

    feat: create and display a family members array

---
+++ 01-create-array.js/task-04.js
@@ -0,0 +1,5 @@
+// Create an array containing your family members' names.
+
+let familyMembers = ["Name: Aminul ", "Fathers Name : Ariful Islam ", "Mothers Name: Hasina "];
+
+console.log(familyMembers);
