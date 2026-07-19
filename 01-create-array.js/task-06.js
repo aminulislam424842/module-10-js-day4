4afde98293b10eaabed82c06ec20e72cb53f7eab
Aminul islam <aminulislam424842@gmail.com>
2026-07-19 21:56:42 +0600

    feat: create and display a months array

---
+++ 01-create-array.js/task-06.js
@@ -0,0 +1,5 @@
+// Create an array of your favorite programming languages.
+
+let favoriteProgrammingLanguage = ["JAVA ","PYTHON","JAVASCRIPT"];
+
+console.log(favoriteProgrammingLanguage);
---
+++ 01-create-array.js/task-07.js
@@ -0,0 +1,18 @@
+// Create an array of 12 months.
+
+let months = [
+  "JANUARY ",
+  "FEBRUARY ",
+  "MARCH ",
+  "APRIL ",
+  "MAY ",
+  "JUNE ",
+  "JULY ",
+  "AUGUST ",
+  "SEPTEMBER ",
+  "OCTOBER ",
+  "NOVEMBER",
+  "DECEMBER"
+];
+
+console.log(months);
