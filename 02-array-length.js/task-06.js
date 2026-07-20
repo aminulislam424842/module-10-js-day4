797f745700914d35a18939e9c5b68fc2a43789b7
Aminul islam <aminulislam424842@gmail.com>
2026-07-20 13:01:33 +0600

    feat: count total family members in an array

---
+++ 02-array-length.js/task-06.js
@@ -0,0 +1,7 @@
+// Print the number of family members.
+
+let familyMembers = ["Name: Aminul ", "Fathers Name : Ariful Islam ", "Mothers Name: Hasina "];
+
+let members = familyMembers.length;
+
+console.log(members);
