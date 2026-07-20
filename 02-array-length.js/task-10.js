bc0d8d9c09bc8e1ab1d1434620d22d54a841bc0f
Aminul islam <aminulislam424842@gmail.com>
2026-07-20 13:23:48 +0600

    feat: compare the lengths of two arrays

---
+++ 02-array-length.js/task-10.js
@@ -0,0 +1,10 @@
+// Compare the length of two arrays.
+
+let arr1 = [1, 2, 3, 4];
+let arr2 = [5, 6, 7, 8];
+
+if (arr1.length === arr2.length) {
+  console.log("Both arrays have the same length.");
+} else {
+  console.log("The arrays have different lengths.");
+}
