2e5e5a6c8a79a083eb73b678c76139fabb945e34
Aminul islam <aminulislam424842@gmail.com>
2026-07-20 20:39:49 +0600

    feat: update every element in an array

---
+++ 04-get-set-by-index.js/task-05.js
@@ -0,0 +1,10 @@
+// Update Every Element
+
+let cities =["Rajshahi","Dhaka","Chattogram","Khulna"];
+
+cities[0]="Sylhet";
+cities[1]="Mymensingh";
+cities[2]="Rangpur";
+cities[3]="Barishal";
+
+console.log(cities);
