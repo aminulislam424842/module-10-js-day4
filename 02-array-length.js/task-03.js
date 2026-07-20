ae6960b10adec04fd71f00ada868b609e6c8af4c
Aminul islam <aminulislam424842@gmail.com>
2026-07-20 12:47:10 +0600

    feat: find the total number of months in an array

---
+++ 02-array-length.js/task-03.js
@@ -0,0 +1,20 @@
+// Print the total months from a months array.
+
+let months = [
+  "JANUARY ",
+  "FEBRUARY ",
+  "MARCH ",
+  "APRIL ",
+  "MAY ",
+  "JUNE ",
+  "JULY ",
+  "AUGUST ",
+  "SEPTEMBER ",
+  "OCTOBER ",
+  "NOVEMBER",
+  "DECEMBER"
+];
+
+let totalMonths =months.length;
+
+console.log(totalMonths);
