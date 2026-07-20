cf9032a7cb877a9e0f1af761d363c35999f71812
Aminul islam <aminulislam424842@gmail.com>
2026-07-20 21:00:33 +0600

    feat: add multiple elements to an array using push

---
+++ 05-push-method.js/task-02.js
@@ -0,0 +1,18 @@
+// Task 2: Add Multiple Values
+
+let months = [
+  "JANUARY",
+  "FEBRUARY ",
+  "MARCH ",
+  "APRIL ",
+  "MAY ",
+  "JUNE ",
+  "JULY ",
+  "AUGUST ",
+  "SEPTEMBER ",
+  "OCTOBER ",
+]
+
+months.push("NOVEMBER", "DECEMBER");
+
+console.log(months);
