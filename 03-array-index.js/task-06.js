4f79a310752ccbba3e10d7423841706a16f94d36
Aminul islam <aminulislam424842@gmail.com>
2026-07-20 13:54:30 +0600

    feat: access the first element from multiple arrays

---
+++ 03-array-index.js/task-06.js
@@ -0,0 +1,9 @@
+// Access Index 0 from Different Arrays
+
+let arr1 = ["fruits"];
+let arr2 = ["colors"];
+let arr3 = ["animals"];
+
+console.log(arr1[0]);
+console.log(arr2[0]);
+console.log(arr3[0]);
