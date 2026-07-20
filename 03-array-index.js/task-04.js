189aece433b7463ce94550f98f98b7872fe7abd1
Aminul islam <aminulislam424842@gmail.com>
2026-07-20 13:43:52 +0600

    feat: access and print the first and last elements of an array

---
+++ 03-array-index.js/task-04.js
@@ -0,0 +1,19 @@
+// Print the First & Last Element
+
+let months = [
+  "JANUARY ",
+  "FEBRUARY ",
+  "MARCH ",
+  "APRIL ",
+  "MAY ",
+  "JUNE ",
+  "JULY ",
+  "AUGUST ",
+  "SEPTEMBER ",
+  "OCTOBER ",
+  "NOVEMBER",
+  "DECEMBER"
+];
+
+console.log(months[0]);
+console.log(months[months.length - 1]);
