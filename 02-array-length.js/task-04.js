2885be94924246f45edecf156cb6b96a50fb9e7d
Aminul islam <aminulislam424842@gmail.com>
2026-07-20 12:54:17 +0600

    feat: find the length of a 20-element array

---
+++ 02-array-length.js/task-04.js
@@ -0,0 +1,7 @@
+// Create an array with 20 numbers and print its length.
+
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+let length = number.length;
+
+console.log(length);
