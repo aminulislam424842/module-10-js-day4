ce211c58e0374057792f2fa46bc1265f48735495
Aminul islam <aminulislam424842@gmail.com>
2026-07-22 11:53:59 +0600

    feat: compare shift and pop methods in arrays

---
+++ 07-shift-method.js/task-05.js
@@ -0,0 +1,27 @@
+// Task 5: Check Length & Compare
+
+// shift
+
+let numbers = [1,2,3,4,5];
+
+console.log(numbers.length);
+numbers.shift();
+console.log(numbers.length);
+
+console.log(numbers);
+
+// pop
+
+let number = [1,2,3,4,5];
+
+console.log(number.length);
+number.pop();
+console.log(number.length);
+
+console.log(number)
+
+
+/**
+ * shift() removes the first element from an array.
+ * pop() removes the last element from an array.
+ */
