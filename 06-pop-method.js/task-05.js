1042ece032c00fe9a29c4210d7c96ed1fcf03b1a
Aminul islam <aminulislam424842@gmail.com>
2026-07-22 11:09:20 +0600

    feat: check array length before and after using pop

---
+++ 06-pop-method.js/task-05.js
@@ -0,0 +1,7 @@
+// Task 5: Check Length Before & After Pop
+
+let number = [11,111,22,222,33,333];
+console.log("Length Before Pop :"+number.length);
+
+number.pop();
+console.log("Length After Pop :" +number.length);
