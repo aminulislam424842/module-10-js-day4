42edcca19da4bf4c1ad2baa7fd30fdee50519e99
Aminul islam <aminulislam424842@gmail.com>
2026-07-22 10:58:37 +0600

    feat: remove the last element from multiple arrays using pop

---
+++ 06-pop-method.js/task-02.js
@@ -0,0 +1,10 @@
+// Task 2: Remove from Different Arrays
+
+const colors = ["Black", "Blue", "White", "Green", "Red"];
+const numbers = [1, 2, 3, 4, 5];
+
+const removed = [colors.pop(), numbers.pop()];
+
+console.log("Removed values:", removed);
+console.log("Colors:", colors);
+console.log("Numbers:", numbers);
