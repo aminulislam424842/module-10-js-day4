dd88ae6ede13679853756085a5b202f85b93c5bb
Aminul islam <aminulislam424842@gmail.com>
2026-07-22 10:33:07 +0600

    feat: add values to an array one by one using push

---
+++ 05-push-method.js/task-04.js
@@ -0,0 +1,18 @@
+// Task 4: Push Values One by One
+
+const numbers = [];
+
+numbers.push(1);
+console.log("After push 1:", numbers);
+
+numbers.push(2);
+console.log("After push 2:", numbers);
+
+numbers.push(3);
+console.log("After push 3:", numbers);
+
+numbers.push(4);
+console.log("After push 4:", numbers);
+
+numbers.push(5);
+console.log("After push 5:", numbers);
