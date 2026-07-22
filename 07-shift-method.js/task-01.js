62c84e8cb4da185616a7d912d991a38585dc1fd9
Aminul islam <aminulislam424842@gmail.com>
2026-07-22 11:29:25 +0600

    feat: remove and return the first array element using shift

---
+++ 07-shift-method.js/task-01.js
@@ -0,0 +1,9 @@
+// Task 1: Remove the First Element
+
+let fruits = ["Mango", "Apple", "Banana", "Orange", "Grapes"];
+
+let removed = fruits.shift();
+
+console.log(removed);
+
+console.log(fruits);
