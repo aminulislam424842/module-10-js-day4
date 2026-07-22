ba84bbe15cf22a36777d87c9d0fc19b10bddbb56
Aminul islam <aminulislam424842@gmail.com>
2026-07-22 12:02:29 +0600

    feat: add an element to the beginning of an array using unshift

---
+++ 08-unshift-method.js/task-01.js
@@ -0,0 +1,10 @@
+// Task 1: Add Values at the Beginning
+
+let fruits = ["Apple","Banana","Orange","Grapes"];
+console.log("Before : " + fruits);
+console.log(fruits.length);
+
+
+fruits.unshift("Mango");
+console.log("After: " + fruits);
+console.log(fruits.length);
