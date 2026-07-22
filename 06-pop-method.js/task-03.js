ec66208e479cdfa596cce4126b70f4fdfdedbbed
Aminul islam <aminulislam424842@gmail.com>
2026-07-22 11:01:58 +0600

    feat: remove array elements one by one using pop

---
+++ 06-pop-method.js/task-03.js
@@ -0,0 +1,20 @@
+// Task 3: Pop Multiple Times
+
+const numbers = [1, 2, 3, 4, 5];
+
+console.log("Initial:", numbers);
+
+numbers.pop();
+console.log("After 1st pop:", numbers);
+
+numbers.pop();
+console.log("After 2nd pop:", numbers);
+
+numbers.pop();
+console.log("After 3rd pop:", numbers);
+
+numbers.pop();
+console.log("After 4th pop:", numbers);
+
+numbers.pop();
+console.log("After 5th pop:", numbers);
