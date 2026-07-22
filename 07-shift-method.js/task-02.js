f21383a0c6cd0f7168d19e0a61dd0c17968038a9
Aminul islam <aminulislam424842@gmail.com>
2026-07-22 11:33:48 +0600

    feat: remove the first element from multiple arrays using shift

---
+++ 07-shift-method.js/task-02.js
@@ -0,0 +1,10 @@
+// Task 2: Shift from Different Arrays
+
+const colors = ["Black", "Blue", "White", "Green", "Red"];
+const numbers = [1, 2, 3, 4, 5];
+
+const removed = [colors.shift(), numbers.shift()];
+
+console.log("Removed values:", removed);
+console.log("Colors:", colors);
+console.log("Numbers:", numbers);
