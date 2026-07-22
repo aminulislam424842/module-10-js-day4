730314b8e2738a9b78b10dc5ea72d3c28b941500
Aminul islam <aminulislam424842@gmail.com>
2026-07-22 12:10:32 +0600

    feat: add values to different arrays using unshift

---
+++ 08-unshift-method.js/task-02.js
@@ -0,0 +1,14 @@
+// Task 2: Add Different Types of Values
+
+let color = ["black", "blue"];
+let number = [2, 3];
+let month = ["February", "March"];
+
+color.unshift("red")
+console.log(color);
+
+number.unshift(1);
+console.log(number);
+
+month.unshift("January");
+console.log(month);
