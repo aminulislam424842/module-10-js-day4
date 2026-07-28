e6a9d2e9f00c5851f5755a1e2df1f0dfd0841393
Aminul islam <aminulislam424842@gmail.com>
2026-07-28 18:46:50 +0600

    feat: practice Array.isArray with empty, null, and undefined values

---
+++ 10-array-isarray.js/task-02.js
@@ -0,0 +1,9 @@
+// Task 2: Check Special Values
+
+let empty = [];
+let nul = null;
+let undefinedValues = undefined;
+
+console.log(Array.isArray(empty));
+console.log(Array.isArray(nul));
+console.log(Array.isArray(undefinedValues));
