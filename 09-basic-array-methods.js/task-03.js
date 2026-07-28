87f3886e62410b0483df00593c826c802fc52949
Aminul islam <aminulislam424842@gmail.com>
2026-07-28 17:44:29 +0600

    feat: practice array search using includes

---
+++ 09-basic-array-methods.js/task-03.js
@@ -0,0 +1,7 @@
+// Task 3: Check Different Data Types
+
+let countries = ["indian","bangladesh","japan"];
+let numbers =[1,2,3,4,5,6,7,8];
+
+console.log(countries.includes("bangladesh"));
+console.log(numbers.includes(100));
