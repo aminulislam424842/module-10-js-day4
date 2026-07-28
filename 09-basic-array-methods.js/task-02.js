9776569abb93c3c687055965361c26dd2f489d27
Aminul islam <aminulislam424842@gmail.com>
2026-07-28 17:40:29 +0600

    feat: practice array index lookup using indexOf

---
+++ 09-basic-array-methods.js/task-02.js
@@ -0,0 +1,5 @@
+let fruits = ["Mango", "Apple", "Banana", "Orange", "Grapes"];
+
+console.log(fruits.indexOf("Banana"));
+console.log(fruits.indexOf("Orange"));
+console.log(fruits.indexOf("Pineapple"));
