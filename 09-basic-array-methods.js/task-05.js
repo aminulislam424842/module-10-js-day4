d240242834e7901c12954043511df8b6299070cb
Aminul islam <aminulislam424842@gmail.com>
2026-07-28 18:08:48 +0600

    feat: practice array search using includes and indexOf

---
+++ 09-basic-array-methods.js/task-05.js
@@ -0,0 +1,5 @@
+// Task 5: Use Both includes() and indexOf()
+
+const colors = ["Black", "Blue", "White", "Green", "Red"];
+console.log(colors.includes("Blue"));
+console.log(colors.indexOf("Blue"));
